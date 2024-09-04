418defd0737eced03bf7ab11d794976384d5ae23
RospikaSiregar <rospikasarahsiregar@gmail.com>
2024-09-04 21:27:32 +0700

    telah menyelesaikan tugas

--- T01.java
+++ T01.java
@@ -1,3 +1,17 @@
+// 12S24008 - Rospika Siregar
 // NIM - NAMA
-// NIM - NAMA
+
+import java.util.*;
+import java.lang.Math;
+
+public class Program {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String nama;
+
+        nama = input.nextLine();
+        System.out.println(nama);
+    }
+}
 
